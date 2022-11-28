9d098d2d9a1b2b8139d5e1d05874e6fc2a535ac4
Jony-2002 <thisisjahongeer@gmail.com>
2022-11-28 18:06:14 +0500

    Changed Phone number and text in UserDetails

--- src/components/UserDetails.vue
+++ src/components/UserDetails.vue
@@ -1,12 +1,13 @@
 <template lang="">
   <div class="text-white">
-    <div>
-      <p>+992 93 123 4567</p>
+    <div class="flex items-center gap-6">
+      <p>+992 90 0000 000</p>
+      <p class="text-xl">&darr;</p>
     </div>
     <div class="flex items-center gap-2">
-      <p>Surname</p>
-      <p>Name</p>
-      <p>Last Name</p>
+      <p>Фамилия</p>
+      <p>Имя</p>
+      <p>Отчество</p>
     </div>
   </div>
 </template>
