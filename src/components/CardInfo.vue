b40eb7ba2a74e64baf906b58fa9b829ff0909edc
Jony-2002 <thisisjahongeer@gmail.com>
2022-11-28 17:39:09 +0500

    Created CardInfo component

---
+++ src/components/CardInfo.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div class="my-8">
+    <p class="text-gray-400">just for test</p>
+    <h3 class="text-4xl font-bold my-3">just for test</h3>
+    <a class="text-sky-500 font-semibold" href="#">just for test</a>
+  </div>
+</template>
+<script>
+export default {
+  name: "CardInfo",
+};
+</script>
+<style lang=""></style>
--- src/components/SmallCard.vue
+++ src/components/SmallCard.vue
@@ -1,17 +1,14 @@
 <template lang="">
   <div class="shadow-md w-[500px] max-w-[100%] rounded-xl px-6 bg-white">
-    <p class="text-gray-400">just for test</p>
-    <h3 class="text-4xl font-bold my-3">just for test</h3>
-    <a class="text-sky-500 font-semibold" href="#">just for test</a>
+    <CardInfoVue />
   </div>
 </template>
 <script>
-
-
+import CardInfoVue from './CardInfo.vue';
 export default {
   name: 'SmallCard',
   components: {
-    
+    CardInfoVue
   }
 }
 </script>
