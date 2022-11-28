a487d84c83a1b7199c3e5b35cf33d8cc416e0784
Jony-2002 <thisisjahongeer@gmail.com>
2022-11-28 17:49:53 +0500

    Created BigCard component

---
+++ src/components/BigCard.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div class="shadow-md w-[700px] max-w-[100%] rounded-xl px-6 bg-white">
+    <p class="text-gray-400">Test</p>
+    <h3 class="text-4xl font-bold my-3">Test</h3>
+    <a class="text-sky-500 font-semibold" href="#">Test</a>
+  </div>
+</template>
+<script>
+import CardInfoVue from "./CardInfo.vue";
+
+export default {
+  name: "BigCard",
+  components: {
+    CardInfoVue,
+  },
+};
+</script>
+<style lang=""></style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,16 @@
 <template lang="">
   <main class="px-16 py-7">
     <SmallCardVue />
+    <BigCardVue />
   </main>
 </template>
 <script>
 import SmallCardVue from "./SmallCard.vue";
-
+import BigCardVue from "./BigCard.vue";
 export default {
   components: {
     SmallCardVue,
+    BigCardVue
   },
 };
 </script>
