36aa910d5d46caceeb5aa5c5938693b18d83bb5b
Jony-2002 <thisisjahongeer@gmail.com>
2022-11-28 17:33:47 +0500

    Added some styles with Tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template lang="">
   <div class="min-h-[100vh] bg-[#ececec]">
     <HeaderVue />
+    <MainVue />
   </div>
 </template>
 <script>
 import HeaderVue from "./components/Header.vue";
+import MainVue from "./components/Main.vue";
 export default {
   components: {
     HeaderVue,
+    MainVue,
   },
 };
 </script>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,15 @@
 <template lang="">
-  <div>
-    main
-  </div>
+  <main class="px-16 py-7">
+    <SmallCardVue />
+  </main>
 </template>
 <script>
+import SmallCardVue from "./SmallCard.vue";
+
 export default {
-  
-}
+  components: {
+    SmallCardVue,
+  },
+};
 </script>
-<style lang="">
-  
-</style>
+<style lang=""></style>
